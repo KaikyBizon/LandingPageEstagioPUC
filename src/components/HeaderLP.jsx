981e38075633f06474daeb0c964ae87b32b02eba
KaikyBizon <kaiky.bizon@gmail.com>
2025-06-11 16:16:05 -0300

    Adição de um card com informações no header

--- src/components/HeaderLP.jsx
+++ src/components/HeaderLP.jsx
@@ -17,13 +17,21 @@ export default function HeaderLp() {
         </div>
       </header>
       <CarouselLP />
-      <section className="intro w-full bg-[#002776] h-[400px] flex justify-center items-center">
-        <div className="phrase-intro w-3/4 flex justify-center">
+      <section className="intro w-full bg-[#002776] h-full py-4 flex justify-center items-center">
+        <div className="phrase-intro w-3/4 h-3/4 space-y-2 flex flex-col lg:flex-row justify-around items-center">
           <h2 className="text-2xl sm:text-5xl font-bold text-white">
             Comece sua carreira
             <br /> antes mesmo de se
             <br /> formar.
           </h2>
+          <div className="w-1/2 xl:w-1/3 h-fit p-4 text-[#002776] 2xl:text-lg bg-white rounded-lg">
+            <p>
+              O estágio é o primeiro contato direto do aluno com o mercado de trabalho, unindo a teoria da faculdade com a prática profissional. 
+              Ele desenvolve habilidades e conhecimentos, alinhando a formação acadêmica à missão social da Universidade. 
+              Para isso, a PUC-Campinas oferece suporte a faculdades, empresas e alunos, fornecendo informações e seguindo as leis para estágios 
+              obrigatórios e não obrigatórios.
+            </p>
+          </div>
         </div>
       </section>
     </>
